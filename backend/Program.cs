b1e9746aca2f89eb79b4ef2664b17f2301b76e10
Stephen Nguyen <tungbinhdinh89@gmail.com>
2024-02-22 22:26:59 +0700

    convert enum to string and add migration

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,14 +1,27 @@
+using backend.Core.AutoMapperConfig;
 using backend.Core.Context;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // DB Configuration
 // Setup connection string
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("local"));
 });
+
+// Automapper Configuration
+builder.Services.AddAutoMapper(typeof(AutoMapperConfigProfile));
+
+builder.Services
+ .AddControllers()
+ .AddJsonOptions(options =>
+ {
+     options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+ });
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
