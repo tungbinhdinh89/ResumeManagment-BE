69b66933f5229fb75bc3c1f2b3e3b943af3a8bcc
Stephen Nguyen <tungbinhdinh89@gmail.com>
2024-02-23 15:33:57 +0700

    api update candidate

--- backend/Controllers/CandidateController.cs
+++ backend/Controllers/CandidateController.cs
@@ -99,6 +99,46 @@ namespace backend.Controllers
         }
 
         // Update
+        [HttpPut]
+        [Route("Update/{id}")]
+        public async Task<IActionResult> UpdateCandidate(long id, [FromForm] CandidateCreateDto dto, IFormFile pdfFile)
+        {
+            var candidate = await _context.Candidates.FindAsync(id);
+
+            if (candidate == null)
+            {
+                return NotFound("Candidate not found");
+            }
+
+            if (pdfFile != null)
+            {
+                // Validate and save pdf to server
+                // Update resumeUrl if pdf is valid
+                var fiveMegaByte = 5 * 1024 * 1024;
+                var pdfMineType = "application/pdf";
+
+                if (pdfFile.Length > fiveMegaByte || pdfFile.ContentType != pdfMineType)
+                {
+                    return BadRequest("File is not valid");
+                }
+
+                var resumeUrl = Guid.NewGuid().ToString() + ".pdf";
+                var filePatch = Path.Combine(Directory.GetCurrentDirectory(), "documents", "pdfs", resumeUrl);
+                using (var stream = new FileStream(filePatch, FileMode.Create))
+                {
+                    await pdfFile.CopyToAsync(stream);
+                }
+
+                candidate.ResumeUrl = resumeUrl;
+            }
+
+            // Update other properties if needed
+            _mapper.Map(dto, candidate);
+            _context.Update(candidate);
+            await _context.SaveChangesAsync();
+
+            return Ok("Candidate updated successfully");
+        }
 
         // Delete
     }
